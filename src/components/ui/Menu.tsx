526fb5e2b355b72044822eb285d6154d80a3c10e
azuazu1313 <asnismarts7@gmail.com>
2025-04-22 02:48:30 +0200

    Update Menu.tsx

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -19,15 +19,15 @@ const Menu = ({ list }: MenuProps) => {
 
   return (
     <MotionConfig transition={{ bounce: 0, type: 'tween' }}>
-      <nav className={'relative'}>
-        <ul className={'flex items-center justify-center space-x-2'}>
+      <nav className="relative">
+        <ul className="flex items-center justify-center space-x-2">
           {list?.map((item) => {
             return (
-              <li key={item.id} className={'relative'}>
+              <li key={item.id} className="relative">
                 <a
                   className={`
                     relative flex items-center justify-center rounded px-4 py-2 transition-all text-text
-                    hover:text-primary
+                    md:text-[9px] lg:text-base hover:text-primary
                     ${hovered === item?.id ? 'text-primary' : ''}
                   `}
                   onMouseEnter={() => setHovered(item.id)}
@@ -40,12 +40,12 @@ const Menu = ({ list }: MenuProps) => {
                   <motion.div
                     layout
                     layoutId={`cursor`}
-                    className={'absolute h-0.5 w-full bg-primary'}
+                    className="absolute h-0.5 w-full bg-primary"
                   />
                 )}
                 {item?.dropdown && hovered === item?.id && (
                   <div
-                    className='absolute left-0 top-full'
+                    className="absolute left-0 top-full"
                     onMouseEnter={() => setHovered(item.id)}
                     onMouseLeave={() => setHovered(null)}
                   >
@@ -58,15 +58,15 @@ const Menu = ({ list }: MenuProps) => {
                       style={{
                         borderRadius: '8px',
                       }}
-                      className='mt-4 flex w-64 flex-col rounded bg-white border border-gray-100 shadow-lg'
-                      layoutId={'cursor'}
+                      className="mt-4 flex w-64 flex-col rounded bg-white border border-gray-100 shadow-lg"
+                      layoutId="cursor"
                     >
                       {item?.items?.map((nav) => {
                         return (
                           <motion.a
                             key={`link-${nav?.id}`}
                             href={`${nav?.url}`}
-                            className={'w-full p-4 hover:bg-gray-50 transition-colors text-text hover:text-primary'}
+                            className="w-full p-4 hover:bg-gray-50 transition-colors text-text hover:text-primary md:text-sm lg:text-base"
                           >
                             {nav?.title}
                           </motion.a>
